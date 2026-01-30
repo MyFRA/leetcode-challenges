24c15743294643edab7fefe935c64c7cffa33b5f
MyFRA <tomyntapss@gmail.com>
2026-01-30 14:55:32 +0700

    14-longest-common-prefix

---
+++ typescript/14-longest-common-prefix.ts
@@ -0,0 +1,30 @@
+function longestCommonPrefix(strs: string[]): string {
+    const map = new Map();
+    let stringPrefix = "";
+
+    let amountShortestString = strs[0].length;
+
+    strs.forEach((str) => {
+        if (str.length < amountShortestString) {
+            amountShortestString = str.length;
+        }
+    });
+
+    for (let indexLetter = 0; indexLetter < amountShortestString; indexLetter++) {
+        const map = new Map();
+
+        strs.forEach((str) => {
+            map.set(str[indexLetter], indexLetter);
+        });
+
+        if (map.size == 1) {
+            stringPrefix += strs[0][indexLetter];
+        } else {
+            break;
+        }
+    }
+
+    return stringPrefix;
+}
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
